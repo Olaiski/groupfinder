20d6c986afbeaa0267f44520e48ac690a939db53
Olaiski <kungbinders@gmail.com>
2020-08-29 22:16:42 +0200

    Test

--- app/src/main/java/com/example/groupfinder/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/groupfinder/calendar/CalendarFragment.kt
@@ -26,7 +26,9 @@ class CalendarFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(CalendarViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
+
+
 
 }
